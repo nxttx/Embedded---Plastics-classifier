88ce17d83957919bd3e3bd6e99e4da383c2faced
Robert Boudewijn <robert-boudewijn@hotmail.com>
2022-10-18 15:22:02 +0200

    remove background from picture

--- evd_3/pre_processing/removing_background.py
+++ evd_3/pre_processing/removing_background.py
@@ -1,48 +1,34 @@
-#  import all data of dataset (rock, paper, scissors, hangloose, ignore)
-#  and remove green/blue background
-#  and save it in a new folder
-
 import cv2 as cv
 import numpy as np
 import os
 import glob
 
-# import all data of dataset (rock, paper, scissors, hangloose, ignore)
-# and remove green/blue background
-# and save it in a new folder
+import time # for timing
 
-# ask for the path to the folder with the images
-path = input("Enter the path to the folder with the images: ")
 
-# path to the new folder where the images will be saved
-path_new = path + "_new"
 
-# if folder does not exist, create it
-if not os.path.exists(path_new):
-    os.makedirs(path_new)
+def remove_background(img):
+    ''' 
+        function that makes image into binary image and removes background
+        input: image (Mat object)
+        output: binary image
+    '''
 
-# create a list with all the images
-images = glob.glob(path + "/*.jpg")
-
-# loop over all images
-for image in images:
-    # read the image
-    img = cv.imread(image)
     img = cv.blur(img,(3,3))
 
     # convert to HSV
     img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-    # # define range of blue color in HSV
+    # define range of blue color in HSV
     light_blue = (0,50,0)
     dark_blue = (255,255,255)
 
-    # # Mark pixels outside background color range
+    # Mark pixels outside background color range
     mask = cv.inRange(img_hsv, light_blue, dark_blue)
     res = mask
 
     # remove green sleeve
-    light_green = (15, 0, 0)
+    light_green = (13, 0, 0)
     dark_green = (127, 255, 255)
 
     # Mark pixels outside background color range
@@ -55,23 +41,73 @@ for image in images:
     # combine the two masks
     res = cv.bitwise_and(res, res2)
 
+    '''
+        if blob is too small, remove it
+    '''
+    #  find contours
+    contours, hierarchy = cv.findContours(res, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    #  find the biggest area
+    max_area = 0
+    for i in range(len(contours)):
+        cnt = contours[i]
+        area = cv.contourArea(cnt)
+        if (area > max_area):
+            max_area = area
+
+    #  if the area is less than 100 pixels, remove it
+    for i in range(len(contours)):
+        cnt = contours[i]
+        area = cv.contourArea(cnt)
+        if (area < 10000):
+            cv.drawContours(res, contours, i, 0, -1)
+
+
+    return res
+
+
+
+
+
+# ask for the path to the folder with the images
+# path = input("Enter the path to the folder with the images: ")
+path = 'C:/Users/Rober/Documents/1.0 ICA/Leerjaar 4/Minor/Project/repository/Embedded---Plastics-classifier/evd_3/original_dataset/ignore'
+
+# path to the new folder where the images will be saved
+path_new = path + "_temp"
+
+# if folder does not exist, create it
+if not os.path.exists(path_new):
+    os.makedirs(path_new)
+
+# create a list with all the images
+images = glob.glob(path + "/*.jpg")
+
+# loop over all images
+for image in images:
+    # read the image
+    img = cv.imread(image)
+    res = remove_background(img)
+
     # save the image
-    cv.imwrite(path_new + "/" + os.path.basename(image), res)
+    # cv.imwrite(path_new + "/" + os.path.basename(image), res)
+
+    # save image with timestamp
+    cv.imwrite(path_new + "/" + os.path.basename(image) + str(time.time())  + ".jpg", res)
+
 
     # show the image
     cv.imshow("image", res)
+
+    # check if the user wants to continue 
     key = cv.waitKey(5)
-    # if key == 27: # wait for ESC key to exit
-    #     break
+    if key == 27: # ESC
+        break
 
 
-
-
-# # show the images
-# cv.imshow('image', img)
-
-# cv.waitKey(0)
+# clean up
 cv.destroyAllWindows()
+
 
 
 
