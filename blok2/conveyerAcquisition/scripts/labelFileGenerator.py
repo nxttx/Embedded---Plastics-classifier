0f1f0521eb798a4ccd06e8c392105afd120d1e28
Robert Boudewijn <robert-boudewijn@hotmail.com>
2022-11-24 20:22:01 +0100

    basis of labelFileGenerator

--- blok2/conveyerAcquisition/scripts/labelFileGenerator.py
+++ blok2/conveyerAcquisition/scripts/labelFileGenerator.py
@@ -30,6 +30,39 @@ def remove_background(src):
     Returns:
         {Mat} The image with the background removed.
     '''
+
+    # find first whiteish line
+    for i in range(src.shape[0]):	
+        if np.sum(src[i, :, :]) > 255 * 3 * 100:
+            break
+
+    # remove line and everything above it
+    src[:i+30, :, :] = 0
+
+    # find last whiteish line
+    for i in range(src.shape[0] - 1, 0, -1):
+        if np.sum(src[i, :, :]) > 255 * 3 * 100:
+            break 
+
+    # remove line and everything below
+    src[i-30:, :, :] = 0
+
+    # small gaussian blur
+    src = cv2.GaussianBlur(src, (3,3), 0)
+
+    # simplefy image to 8bit colors
+    src = cv2.convertScaleAbs(src, alpha=0.03)
+    
+
+    # add laplacian filter
+    src = cv2.Laplacian(src, cv2.CV_8U)
+
+    # histogram equalization
+    src = cv2.cvtColor(src, cv2.COLOR_BGR2YUV)
+    src[:, :, 0] = cv2.equalizeHist(src[:, :, 0])
+    src = cv2.cvtColor(src, cv2.COLOR_YUV2BGR)
+
+
     return src
 
 
@@ -44,7 +77,30 @@ def get_bounding_box(src):
         Output: {int[]} - The bounding box coordinates.   
     '''
 
-    return (0, 0, 0, 0)
+    src = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+    # find the contours in the image
+    contours, hierarchy = cv2.findContours(src, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # find the biggest contour
+    max_area = 0
+    for i in range(len(contours)):
+        cnt = contours[i]
+        area = cv2.contourArea(cnt)
+        if area > max_area:
+            max_area = area
+            max_cnt = cnt
+
+    # get the bounding box of the biggest contour + some padding
+    x, y, w, h = cv2.boundingRect(max_cnt)
+    x -= 10
+    y -= 10
+    w += 20
+    h += 20
+
+    # return the bounding box
+    return [x, y, w, h]
+
 
 
 if __name__ == "__main__":
@@ -69,13 +125,22 @@ if __name__ == "__main__":
     styrofoam_images = os.listdir(styrofoam_dir)
 
     # loop over every image and generate label file
-    for image in bag_images:
-        img = cv2.imread(os.path.join(bag_dir, image))
+    for image in knife_images:
+        img = cv2.imread(os.path.join(knife_dir, image))
         cv2.imshow("org", img)
-        img = remove_background(img)
-        cv2.imshow("WB", img)
-        cv2.waitKey(0)
-        # x, y, w, h = get_bounding_box(img)
+        WB = remove_background(img)
+        cv2.imshow("WB", WB)
+        x, y, w, h = get_bounding_box(WB)
+
+        # draw bounding box on image
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(WB, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imshow("WBBB", WB)
+
+        cv2.imshow("BB", img)
+        x = cv2.waitKey(0)
+        if x == ord("q"):
+            break
 
         # # now write label file
         # with open(os.path.join(bag_dir, image.replace('.jpg', '.txt')), 'w') as f:
