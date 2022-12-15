fc75e6b79e6d7ea3181f22a2ba02b296da407419
Shinichi Hezemans <shinichi@MSI.localdomain>
2022-12-15 12:20:02 +0100

    added custom detect to index.py

--- blok2/conveyerAcquisition/targetApplication/index.py
+++ blok2/conveyerAcquisition/targetApplication/index.py
@@ -2,6 +2,18 @@
 from webserver import WebServer
 import threading
 import time
+import sys
+import os
+import cv2
+# hacky way to import from parent directory
+sys.path.insert(0, os.path.join("..", "transferlearn", "yolov5"))
+from custom_detect import run_custom
+
+
+def callback(img, results):
+    cv2.imshow("img", img)
+    print(results)
+
 
 # create a new thread for the webserver
 webserverThread = threading.Thread(target=WebServer)
@@ -11,3 +23,5 @@ webserverThread.start()
 time.sleep(1)
 
 # import the rest of the modules
+run_custom(os.path.join("blok2", "conveyerAcquisition", "transferlearn",
+           "weigths_of_training", "weights", "best00.15.pt"), 0, callback)
