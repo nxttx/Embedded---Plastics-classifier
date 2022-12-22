60f65da0a10ee97c206f15654d84d939b01bf245
Shinichi Hezemans <shinichi@MSI>
2022-12-22 16:07:43 +0100

    little hack to circumvene devide by zero error

--- blok2/conveyerAcquisition/transferlearn/yolov5/custom_detect.py
+++ blok2/conveyerAcquisition/transferlearn/yolov5/custom_detect.py
@@ -217,6 +217,10 @@ def run(
         LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
 
     # Print results
+    # little temp hack
+    if seen == 0:
+        seen = 1
+
     t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
     LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
     if save_txt or save_img:
