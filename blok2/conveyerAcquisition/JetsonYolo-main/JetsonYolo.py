590db16df804a0718fb9c3ad0182f22082bd079b
Robert Boudewijn <robert-boudewijn@hotmail.com>
2023-01-10 11:59:42 +0100

    update jetson yolo

--- blok2/conveyerAcquisition/JetsonYolo-main/JetsonYolo.py
+++ blok2/conveyerAcquisition/JetsonYolo-main/JetsonYolo.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from ueyeCustom import initialize_ueye_cam, get_ueye_image, close_ueye_camera
 from elements.yolo import OBJ_DETECTION
 
 Object_classes = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
@@ -15,31 +16,32 @@ Object_classes = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', '
 Object_colors = list(np.random.rand(80,3)*255)
 Object_detector = OBJ_DETECTION('weights/yolov5s.pt', Object_classes)
 
-cap = cv2.VideoCapture(0)
-if cap.isOpened():
-    window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
-    # Window
-    while cv2.getWindowProperty("CSI Camera", 0) >= 0:
-        ret, frame = cap.read()
-        if ret:
-            # detection process
-            objs = Object_detector.detect(frame)
+hcam, mem_ptr, width, height, bitspixel, lineinc = initialize_ueye_cam()
 
-            # plotting
-            for obj in objs:
-                # print(obj)
-                label = obj['label']
-                score = obj['score']
-                [(xmin,ymin),(xmax,ymax)] = obj['bbox']
-                color = Object_colors[Object_classes.index(label)]
-                frame = cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), color, 2) 
-                frame = cv2.putText(frame, f'{label} ({str(score)})', (xmin,ymin), cv2.FONT_HERSHEY_SIMPLEX , 0.75, color, 1, cv2.LINE_AA)
 
-        cv2.imshow("CSI Camera", frame)
-        keyCode = cv2.waitKey(30)
-        if keyCode == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-else:
-    print("Unable to open camera")
+window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
+# Window
+while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+    frame = get_ueye_image(hcam, mem_ptr, width, height, bitspixel, lineinc)
+
+    # detection process
+    objs = Object_detector.detect(frame)
+
+    # plotting
+    for obj in objs:
+        # print(obj)
+        label = obj['label']
+        score = obj['score']
+        [(xmin,ymin),(xmax,ymax)] = obj['bbox']
+        color = Object_colors[Object_classes.index(label)]
+        frame = cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), color, 2) 
+        frame = cv2.putText(frame, f'{label} ({str(score)})', (xmin,ymin), cv2.FONT_HERSHEY_SIMPLEX , 0.75, color, 1, cv2.LINE_AA)
+
+    cv2.imshow("CSI Camera", frame)
+    keyCode = cv2.waitKey(30)
+    if keyCode == ord('q'):
+        break
+
+close_ueye_camera(hcam)
+cv2.destroyAllWindows()
+
