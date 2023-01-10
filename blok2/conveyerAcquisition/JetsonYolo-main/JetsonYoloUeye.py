1bab937e5552b138868cbaeeca4b366322c3ad8b
Robert Boudewijn <robert-boudewijn@hotmail.com>
2023-01-10 12:08:18 +0100

    added our own transferlearn

--- blok2/conveyerAcquisition/JetsonYolo-main/JetsonYoloUeye.py
+++ blok2/conveyerAcquisition/JetsonYolo-main/JetsonYoloUeye.py
@@ -3,18 +3,10 @@ import numpy as np
 from ueyeCustom import initialize_ueye_cam, get_ueye_image, close_ueye_camera
 from elements.yolo import OBJ_DETECTION
 
-Object_classes = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
-                'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-                'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
-                'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
-                'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-                'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
-                'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-                'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
-                'hair drier', 'toothbrush' ]
+Object_classes = ['ignore','Bag', 'Bottle', 'Bottlecap', 'Fork', 'Knife', 'Pen', 'Spoon', 'Styrofoam']
 
 Object_colors = list(np.random.rand(80,3)*255)
-Object_detector = OBJ_DETECTION('weights/yolov5s.pt', Object_classes)
+Object_detector = OBJ_DETECTION('Transferlearn.pt', Object_classes)
 
 hcam, mem_ptr, width, height, bitspixel, lineinc = initialize_ueye_cam()
 
