a707e1f7b2f8854c8cb709da0d4fd53f0f8144fc
Robert Boudewijn <robert-boudewijn@hotmail.com>
2023-01-10 11:50:16 +0100

    made jetson yolo more readable

--- blok2/conveyerAcquisition/JetsonYolo-main/JetsonYolo.py
+++ blok2/conveyerAcquisition/JetsonYolo-main/JetsonYolo.py
@@ -15,36 +15,6 @@ Object_classes = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', '
 Object_colors = list(np.random.rand(80,3)*255)
 Object_detector = OBJ_DETECTION('weights/yolov5s.pt', Object_classes)
 
-def gstreamer_pipeline(
-    capture_width=1280,
-    capture_height=720,
-    display_width=1280,
-    display_height=720,
-    framerate=60,
-    flip_method=0,
-):
-    return (
-        "nvarguscamerasrc ! "
-        "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
-
-
-# To flip the image, modify the flip_method parameter (0 and 2 are the most common)
-print(gstreamer_pipeline(flip_method=0))
 cap = cv2.VideoCapture(0)
 if cap.isOpened():
     window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
